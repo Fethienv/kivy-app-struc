7b6946c2cbd7415fe472e76072dba629d46a5dc3
Fethi BEN ABDELHAMID <fethi.env@gmail.com>
2020-07-17 14:38:43 +0100

    first commit

--- settings.py
+++ settings.py
@@ -7,3 +7,28 @@ Themes =  {
         'Purple' :{"file_name": "purple"},
         'Green'  :{"file_name": "green"},
     }
+
+# Language settings
+allways_use_default_language = False
+
+# Set default language iso code
+default_language = "en_US"
+
+# Languages list
+Languages = [
+    {
+        'name'    : "English",
+        'icon'    : "flag",
+        'iso_code': "en_US",
+    },
+    {
+        'name'    : "Français",
+        'icon'    : "flag",
+        'iso_code': "fr_FR",
+    },
+    {
+        'name'    : "العربية",
+        'icon'    : "flag",
+        'iso_code': "ar_DZ",
+    },
+]
